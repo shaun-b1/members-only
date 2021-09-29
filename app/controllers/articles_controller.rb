bec9a37702d054468c1bf884ccda6973b4a383ca
Shaun MacWilliam <shaun.macwilliam@gmail.com>
2021-09-29 16:25:56 +1000

    Posts set up

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,27 @@
+class ArticlesController < ApplicationController
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      flash[:success] = "Great! Your post has been created!"
+      redirect_to root_path
+    else
+      flash.now[:error] = "Rats! Fix your mistakes, please."
+      render :new
+    end
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+end
